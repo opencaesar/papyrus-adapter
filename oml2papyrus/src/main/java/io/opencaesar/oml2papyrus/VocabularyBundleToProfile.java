e8ebe71e8b98033770828ad7275aa6049b320413
Mohammed Mostafa <mohmiim@gmail.com>
2020-11-30 23:45:23 -0500

    convert all  and update relationship conversion

--- oml2papyrus/src/main/java/io/opencaesar/oml2papyrus/VocabularyBundleToProfile.java
+++ oml2papyrus/src/main/java/io/opencaesar/oml2papyrus/VocabularyBundleToProfile.java
@@ -1,10 +1,8 @@
 package io.opencaesar.oml2papyrus;
 
 import java.io.File;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -24,17 +22,17 @@ import org.eclipse.uml2.uml.Stereotype;
 import org.eclipse.uml2.uml.UMLPackage;
 import org.eclipse.uml2.uml.resource.UMLResource;
 
+import io.opencaesar.oml.Annotation;
 import io.opencaesar.oml.Aspect;
 import io.opencaesar.oml.CardinalityRestrictionKind;
 import io.opencaesar.oml.Entity;
 import io.opencaesar.oml.FeatureProperty;
 import io.opencaesar.oml.ForwardRelation;
 import io.opencaesar.oml.Literal;
-import io.opencaesar.oml.Ontology;
 import io.opencaesar.oml.QuotedLiteral;
-import io.opencaesar.oml.Relation;
 import io.opencaesar.oml.RelationCardinalityRestrictionAxiom;
 import io.opencaesar.oml.RelationEntity;
+import io.opencaesar.oml.RelationRestrictionAxiom;
 import io.opencaesar.oml.ReverseRelation;
 import io.opencaesar.oml.Scalar;
 import io.opencaesar.oml.ScalarProperty;
@@ -60,9 +58,17 @@ public class VocabularyBundleToProfile {
 
 	private Model umlMetaModel;
 	private Map<Entity, Class> converted = new HashMap<>();
-	private Map<Vocabulary,Package> voc2Package = new HashMap<>();
+	private Map<Vocabulary, Package> voc2Package = new HashMap<>();
 
-	public VocabularyBundleToProfile(VocabularyBundle rootOntology, File papyrusFolder, ResourceSet papyrusResourceSet, Logger logger) {
+	private static Set<String> vocsToSkip = new HashSet<>();
+	static {
+		vocsToSkip.add("http://www.eclipse.org/uml2/5.0.0/Types");
+		vocsToSkip.add("http://www.eclipse.org/uml2/5.0.0/UML");
+		vocsToSkip.add("http://www.eclipse.org/uml2/5.0.0/UML-Annotations");
+	}
+
+	public VocabularyBundleToProfile(VocabularyBundle rootOntology, File papyrusFolder, ResourceSet papyrusResourceSet,
+			Logger logger) {
 		this.rootOntology = rootOntology;
 		this.papyrusFolder = papyrusFolder;
 		this.papyrusResourceSet = papyrusResourceSet;
@@ -76,45 +82,153 @@ public class VocabularyBundleToProfile {
 
 		// Get the UML metamodel
 		umlMetaModel = ProfileUtils.getUMLMetamodel(papyrusResourceSet);
-		
+
 		// Create the profile
-		URI profileUri = URI.createFileURI(papyrusFolder.getAbsolutePath() + File.separator + rootOntology.getPrefix()+ '.' + UMLResource.PROFILE_FILE_EXTENSION);
-		Profile profile = ProfileUtils.createProfile(papyrusResourceSet, profileUri, rootOntology.getPrefix(), rootOntology.getIri());
-		logger.info("Profile "+profile.getName()+" was created");
-		
+		URI profileUri = URI.createFileURI(papyrusFolder.getAbsolutePath() + File.separator + rootOntology.getPrefix()
+				+ '.' + UMLResource.PROFILE_FILE_EXTENSION);
+		Profile profile = ProfileUtils.createProfile(papyrusResourceSet, profileUri, rootOntology.getPrefix(),
+				rootOntology.getIri());
+		logger.info("Profile " + profile.getName() + " was created");
+
 		// Populate the profile
 		populateProfile(profile);
 
 		// Define the profile after all elements have been created
 		profile.define();
-		
+
 		return profile.eResource();
 	}
-	
+
 	private void populateProfile(Profile profile) {
-		// Create the stereotypes
-		Collection<StereoTypesInfo> entryPoints = getEntryPoints(rootOntology);
-		
-		for (StereoTypesInfo entryPoint : entryPoints) {
-			Vocabulary voc = entryPoint.vocabulary;
-			Entity entity = entryPoint.entity;
-			Package pkg = getPackageForVoc(voc, profile);
-			Stereotype stereotype = ProfileUtils.createStereotype(pkg,
-																  entity.getName(),
-																  entity instanceof Aspect,
-																  entryPoint.metaClasses);
-			logger.debug("Stereotype "+stereotype.getName()+" was created");
+
+		List<Vocabulary> allVoc = OmlRead.getAllImportedOntologies(rootOntology).stream()
+				.filter(ontology -> ontology instanceof Vocabulary).map(ontology -> (Vocabulary) ontology)
+				.collect(Collectors.toList());
+		// now we have all direct voc let's find the stereo types CI
+		for (Vocabulary voc : allVoc) {
+			logger.debug("Converting  : " + voc.getIri());
+			// create the package for the voc
+			if (vocsToSkip.contains(voc.getIri())) {
+				logger.debug("Skipping");
+				continue;
+			}
+			Package pkg = null;
+			// get all voc entities
+			List<Entity> entities = voc.getOwnedStatements().stream().filter(statement -> {
+				return statement instanceof Entity;
+			}).map(statement -> (Entity) statement).collect(Collectors.toList());
+			// go over all entities
+			if (!entities.isEmpty()) {
+				pkg = getPackageForVoc(voc, profile);
+			}
+			for (Entity entity : entities) {
+				logger.debug("Converting : " + entity.getName());
+				StereoTypesInfo infoHolder = getStereoTypeInfo(voc, entity);
+				if (infoHolder != null) {
+					// SteroType
+					Stereotype stereotype = ProfileUtils.createStereotype(pkg, entity.getName(),
+							entity instanceof Aspect, infoHolder.metaClasses);
+					logger.debug("Stereotype " + stereotype.getName() + " was created");
+				}
+				converEntity(profile, entity);
+			}
+			logger.debug("================================================");
 		}
-		
-		for (StereoTypesInfo entryPoint : entryPoints) {
-			Entity entity = entryPoint.entity;
-			converEntity(profile, entity);
-		}
-		
+
+		// Relationships
+		updateRelationships(allVoc);
+
 		// update the generalizations
 		updateAllGeneralizations();
 	}
-	
+
+	private void updateRelationships(List<Vocabulary> allVoc) {
+		for (Vocabulary voc : allVoc) {
+			logger.info("Converting  : " + voc.getIri());
+			// create the package for the voc
+			if (vocsToSkip.contains(voc.getIri())) {
+				logger.info("Skipping");
+				continue;
+			}
+			// get all voc entities
+			List<RelationEntity> entities = voc.getOwnedStatements().stream().filter(statement -> {
+				return statement instanceof RelationEntity;
+			}).map(statement -> (RelationEntity) statement).collect(Collectors.toList());
+			for (RelationEntity entity : entities) {
+				logger.info("Converting Relation: " + entity.getName());
+				logger.info(entity);
+				ForwardRelation srcRel = entity.getForwardRelation();
+				ReverseRelation trgRel = entity.getReverseRelation();
+				Entity src = srcRel.getDomain();
+				Entity trgt = srcRel.getRange();
+				Class srcClass = converted.get(src);
+				Class trgClass = converted.get(trgt);
+				boolean isFunctional = entity.isFunctional();
+				String end1Name = srcRel.getName();
+				boolean end1Navigable = true;
+				String end2Name = "";
+				boolean end2Navigable = false;
+				int end1Lower = 0;
+				int end1Upper = -1;
+				int end2Lower = 0;
+				int end2Upper = -1;
+				if (isFunctional) {
+					end2Upper = 1;
+				}
+				List<RelationRestrictionAxiom> srcCard = OmlSearch.findRelationRestrictionAxiomsWithRelation(srcRel);
+				for (RelationRestrictionAxiom axiom : srcCard) {
+					if (axiom instanceof RelationCardinalityRestrictionAxiom) {
+						RelationCardinalityRestrictionAxiom cardAxiom = (RelationCardinalityRestrictionAxiom) axiom;
+						if (cardAxiom.getKind() == CardinalityRestrictionKind.MIN) {
+							end1Lower = (int) cardAxiom.getCardinality();
+						} else if (cardAxiom.getKind() == CardinalityRestrictionKind.MAX) {
+							end1Upper = (int) cardAxiom.getCardinality();
+						}
+					}
+				}
+				if (trgRel != null) {
+					end2Name = trgRel.getName();
+					end2Navigable = true;
+					List<RelationRestrictionAxiom> trgtCard = OmlSearch
+							.findRelationRestrictionAxiomsWithRelation(trgRel);
+					for (RelationRestrictionAxiom axiom : trgtCard) {
+						if (axiom instanceof RelationCardinalityRestrictionAxiom) {
+							RelationCardinalityRestrictionAxiom cardAxiom = (RelationCardinalityRestrictionAxiom) axiom;
+							if (cardAxiom.getKind() == CardinalityRestrictionKind.MIN) {
+								end2Lower = (int) cardAxiom.getCardinality();
+							} else if (cardAxiom.getKind() == CardinalityRestrictionKind.MAX) {
+								end2Upper = (int) cardAxiom.getCardinality();
+							}
+						}
+					}
+				}
+				srcClass.createAssociation(end1Navigable, AggregationKind.NONE_LITERAL, end1Name, end1Lower, end1Upper,
+						trgClass, end2Navigable, AggregationKind.NONE_LITERAL, end2Name, end2Lower, end2Upper);
+			}
+		}
+	}
+
+	private StereoTypesInfo getStereoTypeInfo(Vocabulary voc, Entity entity) {
+		StereoTypesInfo infoHolder = null;
+		List<Annotation> annotations = OmlSearch.findAnnotations(entity);
+		for (Annotation annotation : annotations) {
+			if (IS_STEREOTYPE.equals(OmlRead.getIri(annotation.getProperty()))) {
+				infoHolder = new StereoTypesInfo(voc, entity);
+			} else if (IS_STEREOTYPE_OF.equals(OmlRead.getIri(annotation.getProperty()))) {
+				Literal value = annotation.getValue();
+				if (value instanceof QuotedLiteral) {
+					QuotedLiteral qLiteral = (QuotedLiteral) value;
+					String sValue = qLiteral.getValue();
+					if (infoHolder == null) {
+						infoHolder = new StereoTypesInfo(voc, entity);
+					}
+					infoHolder.addMetaClass(sValue.substring(sValue.indexOf(':') + 1));
+				}
+			}
+		}
+		return infoHolder;
+	}
+
 	private void updateAllGeneralizations() {
 		Set<Entity> keys = converted.keySet();
 		for (Entity entity : keys) {
@@ -122,16 +236,14 @@ public class VocabularyBundleToProfile {
 			List<SpecializableTerm> specTerms = OmlSearch.findSpecializedTerms(entity);
 			for (SpecializableTerm term : specTerms) {
 				if (term instanceof Entity) {
-					Entity superEntity = (Entity)term;
+					Entity superEntity = (Entity) term;
 					Class superClazz = converted.get(superEntity);
 					clazz.createGeneralization(superClazz);
 				}
 			}
-			
 		}
-		
 	}
-	
+
 	private Package getPackageForVoc(Vocabulary voc, Profile profile) {
 		Package pkg = voc2Package.get(voc);
 		if (pkg == null) {
@@ -140,28 +252,24 @@ public class VocabularyBundleToProfile {
 		}
 		return pkg;
 	}
-	
+
 	private Package getPackage(String iri, Package pkg) {
 		int i = iri.lastIndexOf("/");
-		if (iri.length() == i+1) {
+		if (iri.length() == i + 1) {
 			return pkg;
 		}
 		if (i > 0) {
 			pkg = getPackage(iri.substring(0, i), pkg);
 		}
-		String name = iri.substring(i+1);
+		String name = iri.substring(i + 1);
 		Package newPkg = (Package) pkg.getPackagedElement(name, false, UMLPackage.Literals.PACKAGE, false);
 		if (newPkg == null) {
 			newPkg = ProfileUtils.createPackage(pkg, name, iri);
 		}
 		return newPkg;
 	}
-	
+
 	private Class converEntity(Profile profile, Entity entity) {
-		return converEntity(profile, entity, true);
-	}
-	
-	private Class converEntity(Profile profile, Entity entity, boolean handleSpecilizations) {
 		if (converted.containsKey(entity)) {
 			return converted.get(entity);
 		}
@@ -170,109 +278,37 @@ public class VocabularyBundleToProfile {
 		if (clazz == null) {
 			clazz = ProfileUtils.createClass(containerPackage, entity.getName(), entity instanceof Aspect);
 		}
-		converted.put(entity,clazz);
+		converted.put(entity, clazz);
 		mapProperties(profile, clazz, entity);
-		mapRelationShips(profile, clazz,entity);
-		if (handleSpecilizations) {
-			convertSpecializations(profile, entity);
-		}
 		return clazz;
-	}
-	
-
-	private void mapRelationShips(Profile profile, Class clazz, Entity entity) {
-		List<Relation> relations = OmlSearch.findRelationsWithSource(entity);
-		for (Relation relation : relations) {
-			Entity range = relation.getRange();
-			Class clazz1 = converEntity(profile, range);
-			RelationEntity relEntity = null; 
-			if (relation instanceof ForwardRelation) {
-				ForwardRelation fRel = (ForwardRelation)relation;
-				relEntity = fRel.getRelationEntity();
-				System.out.println(fRel);
-			}else if (relation instanceof ReverseRelation) {
-				ReverseRelation revRelation = (ReverseRelation)relation;
-				relEntity = revRelation.getRelationEntity();
-			}
-			
-			if (relEntity!=null) {
-				ForwardRelation fwdRel = relEntity.getForwardRelation();
-				ReverseRelation revRel = relEntity.getReverseRelation();
-				boolean isFunctional = relEntity.isFunctional();
-				String end1Name = fwdRel.getName();
-				boolean end1Navigable = true;
-				String end2Name = null;
-				boolean end2Navigable = false;
-				if (revRel!=null) {
-					end2Name = revRel.getName();
-					end2Navigable = true;
-				}
-				int end1Lower = 0 ;
-				int end1Upper = -1;
-				if (isFunctional) {
-					end1Upper = 1;
-				}
-				
-				List<RelationCardinalityRestrictionAxiom> card = OmlSearch.findRelationCardinalityRestrictionAxiomsWithRange(relEntity);
-				for(RelationCardinalityRestrictionAxiom axiom : card) {
-					if(axiom.getKind()==CardinalityRestrictionKind.MIN) {
-						end1Lower = (int)axiom.getCardinality();
-					}else if (axiom.getKind()==CardinalityRestrictionKind.MAX) {
-						end1Upper = (int)axiom.getCardinality();
-					}
-				}
-
-				int end2Lower = 0 ;
-				int end2Upper = 1;
-				clazz.createAssociation(end1Navigable, AggregationKind.NONE_LITERAL, end1Name, end1Lower, end1Upper, clazz1,
-										end2Navigable, AggregationKind.NONE_LITERAL, end2Name, end2Lower, end2Upper);	
-			}
-		}
-		
 	}
 
 	private void mapProperties(Package pkg, Class clazz, Entity entity) {
-		 List<FeatureProperty> props = OmlIndex.findFeaturePropertiesWithDomain(entity);
-		 for (FeatureProperty prop : props) {
-			 if (prop instanceof ScalarProperty) {
-				 ScalarProperty sProp = (ScalarProperty)prop;
-				 Scalar range = sProp.getRange();
-				 PrimitiveType rangeClass = getTypeForRange(pkg,range);
-				 clazz.createOwnedAttribute(prop.getName(),rangeClass);
-				 System.out.println("Range : " + range.getName());
-			 }else if (prop instanceof StructuredProperty) {
-				 StructuredProperty stProp = (StructuredProperty)prop;
-				 Structure range = stProp.getRange();
-				 System.out.println("Range : " + range.getName());
-				 
-			 } 
-			 System.out.println(prop.getName());
-		 }
-		
+		List<FeatureProperty> props = OmlIndex.findFeaturePropertiesWithDomain(entity);
+		for (FeatureProperty prop : props) {
+			if (prop instanceof ScalarProperty) {
+				ScalarProperty sProp = (ScalarProperty) prop;
+				Scalar range = sProp.getRange();
+				PrimitiveType rangeClass = getTypeForRange(pkg, range);
+				clazz.createOwnedAttribute(prop.getName(), rangeClass);
+			} else if (prop instanceof StructuredProperty) {
+				StructuredProperty stProp = (StructuredProperty) prop;
+				Structure range = stProp.getRange();
+			}
+		}
+
 	}
 
 	private PrimitiveType getTypeForRange(Package pkg, Scalar range) {
 		switch (range.getName()) {
 		case "string":
-			return (PrimitiveType)umlMetaModel.getMember("String");
+			return (PrimitiveType) umlMetaModel.getMember("String");
 		case "integer":
-			return (PrimitiveType)umlMetaModel.getMember("Integer");
+			return (PrimitiveType) umlMetaModel.getMember("Integer");
 		case "boolean":
-			return (PrimitiveType)umlMetaModel.getMember("Boolean");
+			return (PrimitiveType) umlMetaModel.getMember("Boolean");
 		}
 		return null;
-	}
-
-	private void convertSpecializations(Profile profile, Entity entity) {
-		List<SpecializableTerm> specTerms = OmlSearch.findAllSpecializedTerms(entity);
-		for (SpecializableTerm term : specTerms) {
-			if (term instanceof Entity) {
-				Entity superEntity = (Entity)term;
-				// get the super entity package
-				converEntity(profile, superEntity, false);
-				logger.debug(superEntity.getName());
-			}
-		}
 	}
 
 	private static class StereoTypesInfo {
@@ -300,48 +336,4 @@ public class VocabularyBundleToProfile {
 			metaClasses.add(metaClassName);
 		}
 	}
-
-	/**
-	 * return all annotated Entities as Stereo types
-	 * 
-	 * @param rootOntology the root Ontology
-	 * @return all entry points represented as StereoType Entry
-	 */
-	private Collection<StereoTypesInfo> getEntryPoints(Ontology rootOntology) {
-		List<Vocabulary> directVoc = OmlRead.getAllImportedOntologies(rootOntology).stream()
-				.filter(ontology -> ontology instanceof Vocabulary).map(ontology -> (Vocabulary) ontology)
-				.collect(Collectors.toList());
-		Map<Entity, StereoTypesInfo> entityToInfo = new LinkedHashMap<>();
-
-		// now we have all direct voc let's find the stereo types CI
-		for (Vocabulary voc : directVoc) {
-			// get all voc entities
-			List<Entity> entities = voc.getOwnedStatements().stream().filter(statement -> {
-				return statement instanceof Entity;
-			}).map(statement -> (Entity) statement).collect(Collectors.toList());
-			// not all entities will be sterotyped we need to find the sterotypes ones
-			for (Entity entity : entities) {
-				// find it is annotations
-				OmlSearch.findAnnotations(entity).forEach(annotation -> {
-					if (IS_STEREOTYPE.equals(OmlRead.getIri(annotation.getProperty()))) {
-						entityToInfo.put(entity, new StereoTypesInfo(voc, entity));
-					} else if (IS_STEREOTYPE_OF.equals(OmlRead.getIri(annotation.getProperty()))) {
-						Literal value = annotation.getValue();
-						if (value instanceof QuotedLiteral) {
-							QuotedLiteral qLiteral = (QuotedLiteral) value;
-							String sValue = qLiteral.getValue();
-							StereoTypesInfo info = entityToInfo.get(entity);
-							if (info == null) {
-								info = new StereoTypesInfo(voc, entity);
-								entityToInfo.put(entity, info);
-							}
-							info.addMetaClass(sValue.substring(sValue.indexOf(':') + 1));
-						}
-					}
-				});
-			}
-		}
-		return entityToInfo.values();
-	}
-
 }
