5cba1db2abe14f2bd537d41a4528d960fadc4737
miim <miim@host.docker.internal>
2021-02-23 00:24:53 -0500

    handle EAttribute and ERef

--- papyrus2oml/src/main/java/io/opencaesar/papyrus2oml/converters/ConceptInstanceConverter.java
+++ papyrus2oml/src/main/java/io/opencaesar/papyrus2oml/converters/ConceptInstanceConverter.java
@@ -3,25 +3,83 @@ package io.opencaesar.papyrus2oml.converters;
 import java.util.List;
 
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EAnnotation;
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.uml2.uml.Classifier;
 import org.eclipse.uml2.uml.PackageableElement;
 import org.eclipse.uml2.uml.Property;
+import org.eclipse.uml2.uml.Stereotype;
 
 import io.opencaesar.oml.ConceptInstance;
 import io.opencaesar.oml.Description;
+import io.opencaesar.oml.Literal;
 import io.opencaesar.oml.Member;
 import io.opencaesar.oml.util.OmlRead;
 import io.opencaesar.papyrus2oml.util.ResourceConverter.ConversionContext;
 
 public class ConceptInstanceConverter {
+	
+	private static final String IRI_VALUE = "iri_value";
+	private static final String OMLIRI = "http://io.opencaesar.oml/omliri";
 
-	static public void convert(PackageableElement element, Description description, List<Member> types,
+	static public void convert(PackageableElement element, Description description, List<Stereotype> stereotypes, List<Member> types,
 			ConversionContext context) {
 		ConceptInstance instance = context.writer.addConceptInstance(description, element.getName());
 		context.umlToOml.put(element, instance);
 		String instanceIri = OmlRead.getIri(instance);
 		for (Member t : types) {
 			context.writer.addConceptTypeAssertion(description, instanceIri, OmlRead.getIri(t));
+		}
+	
+		// get the stereoType applications
+		for (Stereotype stereoType : stereotypes) {
+			EObject stApplication = element.getStereotypeApplication(stereoType);
+			EClass eClass = stApplication.eClass();
+			EList<Property> props = stereoType.allAttributes();
+			for (Property prop : props) {
+				if (context.shouldFilterFeature(prop)) {
+					continue;
+				}
+				EStructuralFeature feature = eClass.getEStructuralFeature(prop.getName());
+				if (feature==null) {
+					continue;
+				}
+				Object val = stApplication.eGet(feature);
+				if (prop.isMultivalued()) {
+					EList<?> values = (EList<?>) val;
+					if (!values.isEmpty()) {
+						String propIRI = getIri(prop);
+						if (propIRI.isEmpty()) {
+							context.logger.error("Could not get IRI for " + prop.getName());
+							continue;
+						}
+						if (feature instanceof EAttribute) {
+							for (Object value  : values) {
+								Literal literal = context.getLiteralValue(description, value);
+								context.writer.addScalarPropertyValueAssertion(description, instanceIri, propIRI, literal);
+							}
+						}else {
+							context.deferred.add(new LinkConverter(description, instanceIri, propIRI, val, context ));
+						}
+					}
+				}else if (val!=null) {
+					String propIRI = getIri(prop);
+					if (propIRI.isEmpty()) {
+						context.logger.error("Could not get IRI for " + prop.getName());
+						continue;
+					}
+					if (feature instanceof EAttribute) {
+						Literal literal = context.getLiteralValue(description, val);
+						context.writer.addScalarPropertyValueAssertion(description, instanceIri, propIRI, literal);
+					}else {
+						context.deferred.add(new LinkConverter(description, instanceIri, propIRI, val, context ));
+					}
+				}
+				
+			}
 		}
 		
 		if (element instanceof Classifier) {
@@ -31,6 +89,14 @@ public class ConceptInstanceConverter {
 				System.out.println(prop.getLabel());
 			}
 		}
+	}
+	
+	static public String getIri(Property prop) {
+		EAnnotation annotation = prop.getEAnnotation(OMLIRI);
+		if (annotation!=null) {
+			return annotation.getDetails().get(IRI_VALUE);
+		}
+		return "";
 	}
 
 }
---
+++ papyrus2oml/src/main/java/io/opencaesar/papyrus2oml/converters/LinkConverter.java
@@ -0,0 +1,56 @@
+package io.opencaesar.papyrus2oml.converters;
+
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.uml2.uml.Element;
+import org.eclipse.uml2.uml.util.UMLUtil;
+
+import io.opencaesar.oml.Description;
+import io.opencaesar.oml.IdentifiedElement;
+import io.opencaesar.oml.Ontology;
+import io.opencaesar.oml.util.OmlRead;
+import io.opencaesar.papyrus2oml.util.OMLUtil;
+import io.opencaesar.papyrus2oml.util.ResourceConverter.ConversionContext;
+
+public class LinkConverter implements Runnable {
+	private ConversionContext context;
+	private Description description;
+	private String instanceIri;
+	private String relationIri;
+	private Object target;
+	
+	public LinkConverter(Description description,String instanceIri, String relationIri, Object target , ConversionContext context ) {
+		this.context = context;
+		this.description = description;
+		this.instanceIri = instanceIri;
+		this.relationIri = relationIri;
+		this.target = target;
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public void run() {
+		if (target instanceof List) {
+			List elements = (List)target;
+			for (Object value : elements) {
+				createLink(value);
+			}
+		}else {
+			createLink(target);
+		}
+	}
+
+	private void createLink(Object value) {
+		EObject eValue = (EObject)value;		
+		Element baseElemnt = UMLUtil.getBaseElement(eValue);
+		IdentifiedElement omlElement = context.umlToOml.get(baseElemnt);
+		if (omlElement==null) {
+			return;
+		}
+		Ontology ont = OmlRead.getOntology(omlElement);
+		String targetIri = OmlRead.getIri(omlElement);
+		OMLUtil.addExtendsIfNeeded(description, ont.getIri(), context.writer);
+		context.writer.addLinkAssertion(description, instanceIri, relationIri, targetIri);
+	}
+}
